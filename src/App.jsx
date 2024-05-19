e12130ef7b03c5f715021bfe56ba65465243fdb3
Jecheon Ryu <rjc1704@gmail.com>
2024-05-19 18:03:37 +0900

    계산기 타임어택 작업할 코드베이스

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div>
+      <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
+      <div>
+        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <button>초기화</button>
+      </div>
+      <hr />
+      <div>
+        <h3>결과</h3>
+        <p>0</p>
+      </div>
+    </div>
+  );
+}
